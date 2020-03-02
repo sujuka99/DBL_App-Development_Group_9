fbe51f9f5cada05a403d61ece3db90fed7e45c0c
Laurens <l.p.d.goede@student.tue.nl>
2020-03-02 02:48:39 +0100

    Working login and registration. No actual functionality with your account

--- app/src/main/java/com/example/empty7/MainActivity.java
+++ app/src/main/java/com/example/empty7/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.empty7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.*;  //.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is a test
+        //This is a test 2
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
     }
 }
