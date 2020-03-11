a22852c92f1254d7ba2a0fa5426ff6c6e84dce6c
theodle <50077365+theodle@users.noreply.github.com>
2020-03-11 17:30:05 +0100

    Help page: scrollable TextView. Landscape mode already works well without a separate fragment for it.

--- app/src/main/java/com/example/bq/ui/help/HelpFragment.java
+++ app/src/main/java/com/example/bq/ui/help/HelpFragment.java
@@ -1,6 +1,7 @@
 package com.example.bq.ui.help;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,20 +17,21 @@ import com.example.bq.R;
 
 public class HelpFragment extends Fragment {
 
-    private HelpViewModel helpViewModel;
+    //private HelpViewModel helpViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        helpViewModel =
-                ViewModelProviders.of(this).get(HelpViewModel.class);
+        /**helpViewModel =
+                ViewModelProviders.of(this).get(HelpViewModel.class);*/
         View root = inflater.inflate(R.layout.fragment_help, container, false);
         final TextView textView = root.findViewById(R.id.text_help);
-        helpViewModel.getText().observe(this, new Observer<String>() {
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        /**helpViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
