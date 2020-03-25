d421edbc0a33b294fef85c46dc76f55e1ce2a66e
MathijsMoonen <moonen.mathijs@gmail.com>
2020-03-25 13:07:00 +0100

    hide floating button, fixed landscape mode for available fragments
    Added (for now commented out) code that allows us to hide the floating button that's on every fragment screen. Also fixed landscape mode for our existing pages

--- app/src/main/java/com/example/bq/MainActivity.java
+++ app/src/main/java/com/example/bq/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
+        //fab.hide() uncomment to hide floating button
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
